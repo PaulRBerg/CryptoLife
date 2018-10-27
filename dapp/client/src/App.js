d963fe993a7e1ede13cbb86ad32f9cb3f05ffcc2
Mauer <mauro@osn.global>
2018-10-27 18:17:32 -0300

    Make Counter component

--- dapp/client/src/App.js
+++ dapp/client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
 import truffleContract from "truffle-contract";
 
@@ -9,28 +8,15 @@ import "./css/main.css";
 import Dashboard from './components/Dashboard';
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
-
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
       const web3 = await getWeb3();
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const Contract = truffleContract(SimpleStorageContract);
-      Contract.setProvider(web3.currentProvider);
-      const instance = await Contract.deployed();
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`
+        `Failed to load web3. Check console for details.`
       );
       console.log(error);
     }
